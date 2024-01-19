4ba63bf11b1d9d97202141baca2a0d9d4976b15c
stevkim <stevenn.kkim@gmail.com>
2024-01-19 15:03:35 -0800

    Added a few more animations.

--- src/containers/About.tsx
+++ src/containers/About.tsx
@@ -9,13 +9,19 @@ const About = () => {
     <section className=" w-full min-h-[90vh] flex flex-col p-4 md:px-[20%] text-xl pt-20">
       <h1 className="text-3xl font-semibold text-center">a bit about me.</h1>
       <div className="flex flex-col lg:flex-row">
-        <div className="lg:w-[50%] w-full flex flex-col gap-4 p-8 my-auto">
+        <motion.div
+          initial={{ opacity: 0, x: -200 }}
+          whileInView={{ opacity: 1, x: 0 }}
+          viewport={{ once: true }}
+          className="lg:w-[50%] w-full flex flex-col gap-4 p-8 my-auto"
+        >
           <AboutMe />
           <CurrentStack />
-        </div>
+        </motion.div>
         <motion.div
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
+          initial={{ opacity: 0, x: 200 }}
+          whileInView={{ opacity: 1, x: 0 }}
+          viewport={{ once: true }}
           className="lg:w-[50%] w-full p-20 flex items-start justify-center"
         >
           <SelfPortrait />
